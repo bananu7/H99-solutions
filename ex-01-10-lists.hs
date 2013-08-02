ab30c747848810d82ea9cb3d3f574a3776056c01
Bartek Banachewicz <bananu7@o2.pl>
2013-08-02 21:48:11 +0200

    Added exercise 3

--- ex-01-10-lists.hs
+++ ex-01-10-lists.hs
@@ -18,10 +18,13 @@ elementAt :: (Integral b) => [a] -> b -> a
 elementAt (x:xs) 0 = x
 elementAt (x:xs) ix = elementAt xs (ix-1)
 
-
 --Ex 4
 myLength :: (Integral b) => [a] -> b
 myLength [] = 0
 myLength (x:xs) = myLength xs + 1
 
+--Ex 5
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse xs = foldl (\acc x -> x:acc) [] xs 
 
